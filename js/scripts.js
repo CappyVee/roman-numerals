8b3e4f6d353d545a20a32f57cdbbe753a612581d
Chris Quattrocchi and Lena Ha <chris@>
2017-10-26 09:34:25 -0700

    initialize git, set up directories, and add input function

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+////business logic
+var numberFirst = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var numeralsFirst = ["I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"];
+var numberSecond = ["10", "20", "30", "40", "50", "60", "70", "80", "90"];
+var numeralsSecond = ["X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"];
+var numberThird = ["100", "200", "300", "400", "500", "600", "700", "800", "900"];
+var numeralsThird = ["C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"];
+var numberFourth = ["1000", "2000", "3000"];
+var numeralsFourth = ["M", "MM", "MMM"];
+
+
+
+
+
+//////user logic
+$(document).ready(function(){
+  $("#inputForm").submit(function(event){
+    event.preventDefault();
+    var number = parseInt($("#userInput").val());
+
+    // cannot accept non-integers
+    if (number !== NaN) {
+      var notice = number;
+    } else {
+        var notice = "NOPE!"
+    }
+    alert(notice);
+
+    // cannot accept numbers higher than 3999
+
+    // add value of all symbols
+
+    // no more than three identical symbols in a row
+
+    // separate ones, tens, hundreds, and thousands
+
+  });//event prevent default
+});//end doc ready
