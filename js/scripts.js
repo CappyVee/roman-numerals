648c895904a4cd9a1e0ce3ce19e398c3eeabf696
Chris Quattrocchi and Lena Ha <lena@>
2017-10-26 13:24:43 -0700

    add logic to split number by decimal

--- js/scripts.js
+++ js/scripts.js
@@ -59,17 +59,63 @@ $(document).ready(function(){
     var numberStr = $("#userInput").val();
 
     //cannot accept numbers higher than 3999
-    if (number > 4000 || number < 1) {
-      alert("Please enter a number between 1 and 3999.")
-    }
+    if (number < 1) {
+      alert("enter larger number");
+    } else if (number < 10 ) {
+      var firstNumber = parseInt(numberStr.slice(0,1));
+      alert(firstNumber);
+    } else if (number < 100) {
+      var firstNumber = parseInt(numberStr.slice(0,1));
+      var secondNumber = parseInt(numberStr.slice(1,2));
+      alert(firstNumber + " " + secondNumber);
+    } else if (number < 1000) {
+      var firstNumber = parseInt(numberStr.slice(0,1));
+      var secondNumber = parseInt(numberStr.slice(1,2));
+      var thirdNumber = parseInt(numberStr.slice(2,3));
+      alert(firstNumber + " " + secondNumber + " " + thirdNumber);
+    } else if (number < 4000) {
+      var firstNumber = parseInt(numberStr.slice(0,1));
+      var secondNumber = parseInt(numberStr.slice(1,2));
+      var thirdNumber = parseInt(numberStr.slice(2,3));
+      var fourthNumber = parseInt(numberStr.slice(3,4));
+      alert(firstNumber + " " + secondNumber + " " + thirdNumber + " " + fourthNumber);
+    } else
+    alert("Please enter a number under 4000");
 
-    for (var i = 0; i < numberStr.length; i++) {
-      if (number === 1) {
-      var numeral = numberStr.split("1").join("I");
-       alert(numeral);
-      }
-    }
-
+    // if (number > 4000 || number < 1) {
+    //   alert("Please enter a number between 1 and 3999.")
+    // }
+    //
+    // for (var i = 0; i < numberStr.length; i++) {
+    //   if (number === 1) {
+    //   var numeral = numberStr.split("1").join("I");
+    //    alert(numeral);
+    //  } else if (number === 2) {
+    //    var numeral = numberStr.split("2").join("II");
+    //    alert(numeral);
+    //  } else if (number === 3) {
+    //    var numeral = numberStr.split("3").join("III");
+    //    alert(numeral);
+    //  } else if (number === 4) {
+    //    var numeral = numberStr.split("4").join("IV");
+    //    alert(numeral);
+    //  } else if (number === 5) {
+    //    var numeral = numberStr.split("5").join("V");
+    //    alert(numeral);
+    //  } else if (number === 6) {
+    //    var numeral = numberStr.split("6").join("VI");
+    //    alert(numeral);
+    //  } else if (number === 7) {
+    //    var numeral = numberStr.split("7").join("VII");
+    //    alert(numeral);
+    //  } else if (number === 8) {
+    //    var numeral = numberStr.split("8").join("VIII");
+    //    alert(numeral);
+    //  } else if (number === 9) {
+    //    var numeral = numberStr.split("9").join("IX");
+    //    alert(numeral);
+    //  }
+    // }
     // } else if (number === 2) {
     //
     // }
